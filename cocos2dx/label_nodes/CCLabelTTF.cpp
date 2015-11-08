f35f4a7b56de74fe31321e087ab623f0613547bd
Wasin Thonkaew <haxpor@gmail.com>
2015-11-08 15:07:02 +0700

    Update CCLabelTTF class to old version
    It has flickering problem when we render text in RunningLabelTTF class.

--- cocos2dx/label_nodes/CCLabelTTF.cpp
+++ cocos2dx/label_nodes/CCLabelTTF.cpp
@@ -1,27 +1,27 @@
 /****************************************************************************
-Copyright (c) 2010-2012 cocos2d-x.org
-Copyright (c) 2008-2010 Ricardo Quesada
-
-http://www.cocos2d-x.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+ Copyright (c) 2010-2012 cocos2d-x.org
+ Copyright (c) 2008-2010 Ricardo Quesada
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
 #include "CCLabelTTF.h"
 #include "CCDirector.h"
 #include "shaders/CCGLProgram.h"
@@ -45,15 +45,17 @@ CCLabelTTF::CCLabelTTF()
 , m_pFontName(NULL)
 , m_fFontSize(0.0)
 , m_string("")
-, m_shadowEnabled(false)
-, m_strokeEnabled(false)
-, m_textFillColor(ccWHITE)
 {
 }
 
 CCLabelTTF::~CCLabelTTF()
 {
     CC_SAFE_DELETE(m_pFontName);
+}
+
+CCLabelTTF * CCLabelTTF::node()
+{
+    return CCLabelTTF::create();
 }
 
 CCLabelTTF * CCLabelTTF::create()
@@ -83,23 +85,11 @@ CCLabelTTF * CCLabelTTF::create(const char *string, const char *fontName, float 
 }
 
 CCLabelTTF* CCLabelTTF::create(const char *string, const char *fontName, float fontSize,
-                               const CCSize &dimensions, CCTextAlignment hAlignment, 
+                               const CCSize &dimensions, CCTextAlignment hAlignment,
                                CCVerticalTextAlignment vAlignment)
 {
     CCLabelTTF *pRet = new CCLabelTTF();
     if(pRet && pRet->initWithString(string, fontName, fontSize, dimensions, hAlignment, vAlignment))
-    {
-        pRet->autorelease();
-        return pRet;
-    }
-    CC_SAFE_DELETE(pRet);
-    return NULL;
-}
-
-CCLabelTTF * CCLabelTTF::createWithFontDefinition(const char *string, ccFontDefinition &textDefinition)
-{
-    CCLabelTTF *pRet = new CCLabelTTF();
-    if(pRet && pRet->initWithStringAndTextDefinition(string, textDefinition))
     {
         pRet->autorelease();
         return pRet;
@@ -113,7 +103,7 @@ bool CCLabelTTF::init()
     return this->initWithString("", "Helvetica", 12);
 }
 
-bool CCLabelTTF::initWithString(const char *label, const char *fontName, float fontSize, 
+bool CCLabelTTF::initWithString(const char *label, const char *fontName, float fontSize,
                                 const CCSize& dimensions, CCTextAlignment alignment)
 {
     return this->initWithString(label, fontName, fontSize, dimensions, alignment, kCCVerticalTextAlignmentTop);
@@ -121,7 +111,7 @@ bool CCLabelTTF::initWithString(const char *label, const char *fontName, float f
 
 bool CCLabelTTF::initWithString(const char *label, const char *fontName, float fontSize)
 {
-    return this->initWithString(label, fontName, fontSize, 
+    return this->initWithString(label, fontName, fontSize,
                                 CCSizeZero, kCCTextAlignmentLeft, kCCVerticalTextAlignmentTop);
 }
 
@@ -135,10 +125,10 @@ bool CCLabelTTF::initWithString(const char *string, const char *fontName, float 
         this->setShaderProgram(CCShaderCache::sharedShaderCache()->programForKey(SHADER_PROGRAM));
         
         m_tDimensions = CCSizeMake(dimensions.width, dimensions.height);
-        m_hAlignment  = hAlignment;
-        m_vAlignment  = vAlignment;
-        m_pFontName   = new std::string(fontName);
-        m_fFontSize   = fontSize;
+        m_hAlignment = hAlignment;
+        m_vAlignment = vAlignment;
+        m_pFontName = new std::string(fontName);
+        m_fFontSize = fontSize;
         
         this->setString(string);
         
@@ -147,29 +137,6 @@ bool CCLabelTTF::initWithString(const char *string, const char *fontName, float 
     
     return false;
 }
-
-bool CCLabelTTF::initWithStringAndTextDefinition(const char *string, ccFontDefinition &textDefinition)
-{
-    if (CCSprite::init())
-    {
-        // shader program
-        this->setShaderProgram(CCShaderCache::sharedShaderCache()->programForKey(SHADER_PROGRAM));
-        
-        // prepare everythin needed to render the label
-        _updateWithTextDefinition(textDefinition, false);
-        
-        // set the string
-        this->setString(string);
-        
-        //
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
 
 void CCLabelTTF::setString(const char *string)
 {
@@ -292,276 +259,33 @@ void CCLabelTTF::setFontName(const char *fontName)
 // Helper
 bool CCLabelTTF::updateTexture()
 {
+    bool bRet = false;
     CCTexture2D *tex;
-    tex = new CCTexture2D();
     
-    if (!tex)
-        return false;
-    
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    
-        ccFontDefinition texDef = _prepareTextDefinition(true);
-        tex->initWithString( m_string.c_str(), &texDef );
-    
-    #else
-    
-        tex->initWithString( m_string.c_str(),
-                            m_pFontName->c_str(),
-                            m_fFontSize * CC_CONTENT_SCALE_FACTOR(),
-                            CC_SIZE_POINTS_TO_PIXELS(m_tDimensions),
-                            m_hAlignment,
-                            m_vAlignment);
-    
-    #endif
-    
-    // set the texture
-    this->setTexture(tex);
-    // release it
-    tex->release();
-    
-    // set the size in the sprite
-    CCRect rect =CCRectZero;
-    rect.size   = m_pobTexture->getContentSize();
-    this->setTextureRect(rect);
-    
-    //ok
-    return true;
-}
-
-void CCLabelTTF::enableShadow(const CCSize &shadowOffset, float shadowOpacity, float shadowBlur, bool updateTexture)
-{
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    
-        bool valueChanged = false;
-        
-        if (false == m_shadowEnabled)
-        {
-            m_shadowEnabled = true;
-            valueChanged    = true;
-        }
-        
-        if ( (m_shadowOffset.width != shadowOffset.width) || (m_shadowOffset.height!=shadowOffset.height) )
-        {
-            m_shadowOffset.width  = shadowOffset.width;
-            m_shadowOffset.height = shadowOffset.height;
-            
-            valueChanged = true;
-        }
-        
-        if (m_shadowOpacity != shadowOpacity )
-        {
-            m_shadowOpacity = shadowOpacity;
-            valueChanged = true;
-        }
-
-        if (m_shadowBlur    != shadowBlur)
-        {
-            m_shadowBlur = shadowBlur;
-            valueChanged = true;
-        }
-        
-        
-        if ( valueChanged && updateTexture )
-        {
-            this->updateTexture();
-        }
-    
-    #else
-        CCLOGERROR("Currently only supported on iOS and Android!");
-    #endif
-    
-}
-
-void CCLabelTTF::disableShadow(bool updateTexture)
-{
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    
-        if (m_shadowEnabled)
-        {
-            m_shadowEnabled = false;
-    
-            if (updateTexture)
-                this->updateTexture();
-            
-        }
-    
-    #else
-        CCLOGERROR("Currently only supported on iOS and Android!");
-    #endif
-}
-
-void CCLabelTTF::enableStroke(const ccColor3B &strokeColor, float strokeSize, bool updateTexture)
-{
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    
-        bool valueChanged = false;
-        
-        if(m_strokeEnabled == false)
-        {
-            m_strokeEnabled = true;
-            valueChanged = true;
-        }
-        
-        if ( (m_strokeColor.r != strokeColor.r) || (m_strokeColor.g != strokeColor.g) || (m_strokeColor.b != strokeColor.b) )
-        {
-            m_strokeColor = strokeColor;
-            valueChanged = true;
-        }
-        
-        if (m_strokeSize!=strokeSize)
-        {
-            m_strokeSize = strokeSize;
-            valueChanged = true;
-        }
-        
-        if ( valueChanged && updateTexture )
-        {
-            this->updateTexture();
-        }
-    
-    #else
-        CCLOGERROR("Currently only supported on iOS and Android!");
-    #endif
-    
-}
-
-void CCLabelTTF::disableStroke(bool updateTexture)
-{
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    
-        if (m_strokeEnabled)
-        {
-            m_strokeEnabled = false;
-            
-            if (updateTexture)
-                this->updateTexture();
-        }
-    
-    #else
-        CCLOGERROR("Currently only supported on iOS and Android!");
-    #endif
-    
-}
-
-void CCLabelTTF::setFontFillColor(const ccColor3B &tintColor, bool updateTexture)
-{
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-        if (m_textFillColor.r != tintColor.r || m_textFillColor.g != tintColor.g || m_textFillColor.b != tintColor.b)
-        {
-            m_textFillColor = tintColor;
-            
-            if (updateTexture)
-                this->updateTexture();
-        }
-    #else
-        CCLOGERROR("Currently only supported on iOS and Android!");
-    #endif
-}
-
-void CCLabelTTF::setTextDefinition(ccFontDefinition *theDefinition)
-{
-    if (theDefinition)
+    do
     {
-        _updateWithTextDefinition(*theDefinition, true);
-    }
-}
-
-ccFontDefinition *CCLabelTTF::getTextDefinition()
-{
-    ccFontDefinition *tempDefinition = new ccFontDefinition;
-    *tempDefinition = _prepareTextDefinition(false);
-    return tempDefinition;
-}
-
-void CCLabelTTF::_updateWithTextDefinition(ccFontDefinition & textDefinition, bool mustUpdateTexture)
-{
-    m_tDimensions = CCSizeMake(textDefinition.m_dimensions.width, textDefinition.m_dimensions.height);
-    m_hAlignment  = textDefinition.m_alignment;
-    m_vAlignment  = textDefinition.m_vertAlignment;
-    
-    m_pFontName   = new std::string(textDefinition.m_fontName);
-    m_fFontSize   = textDefinition.m_fontSize;
-    
-    
-    // shadow
-    if ( textDefinition.m_shadow.m_shadowEnabled )
-    {
-        enableShadow(textDefinition.m_shadow.m_shadowOffset, textDefinition.m_shadow.m_shadowOpacity, textDefinition.m_shadow.m_shadowBlur, false);
-    }
-    
-    // stroke
-    if ( textDefinition.m_stroke.m_strokeEnabled )
-    {
-        enableStroke(textDefinition.m_stroke.m_strokeColor, textDefinition.m_stroke.m_strokeSize, false);
-    }
-    
-    // fill color
-    setFontFillColor(textDefinition.m_fontFillColor, false);
-    
-    if (mustUpdateTexture)
-        updateTexture();
-}
-
-ccFontDefinition CCLabelTTF::_prepareTextDefinition(bool adjustForResolution)
-{
-    ccFontDefinition texDef;
-    
-    if (adjustForResolution)
-        texDef.m_fontSize       =  m_fFontSize * CC_CONTENT_SCALE_FACTOR();
-    else
-        texDef.m_fontSize       =  m_fFontSize;
-    
-    texDef.m_fontName       = *m_pFontName;
-    texDef.m_alignment      =  m_hAlignment;
-    texDef.m_vertAlignment  =  m_vAlignment;
-    
-    
-    if (adjustForResolution)
-        texDef.m_dimensions     =  CC_SIZE_POINTS_TO_PIXELS(m_tDimensions);
-    else
-        texDef.m_dimensions     =  m_tDimensions;
-    
-    
-    // stroke
-    if ( m_strokeEnabled )
-    {
-        texDef.m_stroke.m_strokeEnabled = true;
-        texDef.m_stroke.m_strokeColor   = m_strokeColor;
+        // let system compute label's width or height when its value is 0
+        // refer to cocos2d-x issue #1430
+        tex = new CCTexture2D();
+        CC_BREAK_IF(!tex);
         
-        if (adjustForResolution)
-            texDef.m_stroke.m_strokeSize = m_strokeSize * CC_CONTENT_SCALE_FACTOR();
-        else
-            texDef.m_stroke.m_strokeSize = m_strokeSize;
+        bRet = tex->initWithString( m_string.c_str(),
+                                   m_pFontName->c_str(),
+                                   m_fFontSize * CC_CONTENT_SCALE_FACTOR(),
+                                   CC_SIZE_POINTS_TO_PIXELS(m_tDimensions),
+                                   m_hAlignment,
+                                   m_vAlignment);
+        CC_BREAK_IF(!bRet);
         
+        this->setTexture(tex);
+        tex->release();
         
-    }
-    else
-    {
-        texDef.m_stroke.m_strokeEnabled = false;
-    }
+        CCRect rect = CCRectZero;
+        rect.size = m_pobTexture->getContentSize();
+        this->setTextureRect(rect);
+    }while(false);
     
-    
-    // shadow
-    if ( m_shadowEnabled )
-    {
-        texDef.m_shadow.m_shadowEnabled         = true;
-        texDef.m_shadow.m_shadowBlur            = m_shadowBlur;
-        texDef.m_shadow.m_shadowOpacity         = m_shadowOpacity;
-        
-        if (adjustForResolution)
-            texDef.m_shadow.m_shadowOffset = CC_SIZE_POINTS_TO_PIXELS(m_shadowOffset);
-        else
-            texDef.m_shadow.m_shadowOffset = m_shadowOffset;
-    }
-    else
-    {
-        texDef.m_shadow.m_shadowEnabled = false;
-    }
-    
-    // text tint
-    texDef.m_fontFillColor = m_textFillColor;
-    
-    return texDef;
+    return bRet;
 }
 
 NS_CC_END
--- cocos2dx/label_nodes/CCLabelTTF.h
+++ cocos2dx/label_nodes/CCLabelTTF.h
@@ -1,27 +1,27 @@
 /****************************************************************************
-Copyright (c) 2010-2012 cocos2d-x.org
-Copyright (c) 2008-2010 Ricardo Quesada
-
-http://www.cocos2d-x.org
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+ Copyright (c) 2010-2012 cocos2d-x.org
+ Copyright (c) 2008-2010 Ricardo Quesada
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
 #ifndef __CCLABELTTF_H__
 #define __CCLABELTTF_H__
 
@@ -37,39 +37,17 @@ NS_CC_BEGIN
  * @{
  */
 
-
-
 /** @brief CCLabelTTF is a subclass of CCTextureNode that knows how to render text labels
  *
  * All features from CCTextureNode are valid in CCLabelTTF
  *
  * CCLabelTTF objects are slow. Consider using CCLabelAtlas or CCLabelBMFont instead.
- *
- * Custom ttf file can be put in assets/ or external storage that the Application can access.
- * @code
- * CCLabelTTF *label1 = CCLabelTTF::create("alignment left", "A Damn Mess", fontSize, blockSize, 
- *                                          kCCTextAlignmentLeft, kCCVerticalTextAlignmentCenter);
- * CCLabelTTF *label2 = CCLabelTTF::create("alignment right", "/mnt/sdcard/Scissor Cuts.ttf", fontSize, blockSize,
- *                                          kCCTextAlignmentLeft, kCCVerticalTextAlignmentCenter);
- * @endcode
- *
  */
 class CC_DLL CCLabelTTF : public CCSprite, public CCLabelProtocol
 {
 public:
-    /**
-     *  @js ctor
-     */
     CCLabelTTF();
-    /**
-     *  @js NA
-     *  @lua NA
-     */
     virtual ~CCLabelTTF();
-    /**
-     *  @js NA
-     *  @lua NA
-     */
     const char* description();
     
     /** creates a CCLabelTTF with a font name and font size in points
@@ -82,17 +60,13 @@ public:
      */
     static CCLabelTTF * create(const char *string, const char *fontName, float fontSize,
                                const CCSize& dimensions, CCTextAlignment hAlignment);
-  
+    
     /** creates a CCLabel from a fontname, alignment, dimension in points and font size in points
      @since v2.0.1
      */
     static CCLabelTTF * create(const char *string, const char *fontName, float fontSize,
-                               const CCSize& dimensions, CCTextAlignment hAlignment, 
+                               const CCSize& dimensions, CCTextAlignment hAlignment,
                                CCVerticalTextAlignment vAlignment);
-    
-    
-    /** Create a lable with string and a font definition*/
-    static CCLabelTTF * createWithFontDefinition(const char *string, ccFontDefinition &textDefinition);
     
     /** initializes the CCLabelTTF with a font name and font size */
     bool initWithString(const char *string, const char *fontName, float fontSize);
@@ -100,50 +74,25 @@ public:
     /** initializes the CCLabelTTF with a font name, alignment, dimension and font size */
     bool initWithString(const char *string, const char *fontName, float fontSize,
                         const CCSize& dimensions, CCTextAlignment hAlignment);
-
+    
     /** initializes the CCLabelTTF with a font name, alignment, dimension and font size */
     bool initWithString(const char *string, const char *fontName, float fontSize,
-                        const CCSize& dimensions, CCTextAlignment hAlignment, 
+                        const CCSize& dimensions, CCTextAlignment hAlignment,
                         CCVerticalTextAlignment vAlignment);
-    
-    /** initializes the CCLabelTTF with a font name, alignment, dimension and font size */
-    bool initWithStringAndTextDefinition(const char *string, ccFontDefinition &textDefinition);
-    
-    /** set the text definition used by this label */
-    void setTextDefinition(ccFontDefinition *theDefinition);
-    
-    /** get the text definition used by this label */
-    ccFontDefinition * getTextDefinition();
-    
-    
-    
-    /** enable or disable shadow for the label */
-    void enableShadow(const CCSize &shadowOffset, float shadowOpacity, float shadowBlur, bool mustUpdateTexture = true);
-    
-    /** disable shadow rendering */
-    void disableShadow(bool mustUpdateTexture = true);
-    
-    /** enable or disable stroke */
-    void enableStroke(const ccColor3B &strokeColor, float strokeSize, bool mustUpdateTexture = true);
-    
-    /** disable stroke */
-    void disableStroke(bool mustUpdateTexture = true);
-    
-    /** set text tinting */
-    void setFontFillColor(const ccColor3B &tintColor, bool mustUpdateTexture = true);
-
-    
     
     /** initializes the CCLabelTTF */
     bool init();
-
+    /** Creates an label.
+     */
+    static CCLabelTTF * node();
+    
     /** Creates an label.
      */
     static CCLabelTTF * create();
-
+    
     /** changes the string to render
-    * @warning Changing the string is as expensive as creating a new CCLabelTTF. To obtain better performance use CCLabelAtlas
-    */
+     * @warning Changing the string is as expensive as creating a new CCLabelTTF. To obtain better performance use CCLabelAtlas
+     */
     virtual void setString(const char *label);
     virtual const char* getString(void);
     
@@ -165,11 +114,6 @@ public:
 private:
     bool updateTexture();
 protected:
-    
-    /** set the text definition for this label */
-    void                _updateWithTextDefinition(ccFontDefinition & textDefinition, bool mustUpdateTexture = true);
-    ccFontDefinition    _prepareTextDefinition(bool adjustForResolution = false);
-    
     /** Dimensions of the label in Points */
     CCSize m_tDimensions;
     /** The alignment of the label */
@@ -180,25 +124,8 @@ protected:
     std::string * m_pFontName;
     /** Font size of the label */
     float m_fFontSize;
-    /** label's string */
+    
     std::string m_string;
-    
-    /** font shadow */
-    bool    m_shadowEnabled;
-    CCSize  m_shadowOffset;
-    float   m_shadowOpacity;
-    float   m_shadowBlur;
-    
-    
-    /** font stroke */
-    bool        m_strokeEnabled;
-    ccColor3B   m_strokeColor;
-    float       m_strokeSize;
-        
-    /** font tint */
-    ccColor3B   m_textFillColor;
-
-    
 };
 
 
